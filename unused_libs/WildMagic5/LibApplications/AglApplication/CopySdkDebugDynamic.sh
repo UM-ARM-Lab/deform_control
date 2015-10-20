e57fd10a320ff34c8c449d8f18047641c70758da
Dale McConachie <dmcconachie@wpi.edu>
2015-10-20 18:07:21 -0400

    Deleted un-needed libraries.

--- unused_libs/WildMagic5/LibApplications/AglApplication/CopySdkDebugDynamic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/tcsh
-cd ${SRCROOT}
-set HEADERS = `ls ../*.h ../*.inl`
-foreach header (${HEADERS})
-    cp -fp "${SRCROOT}"/$header ../../SDK/Include
-end
-
-cp -fp "${BUILT_PRODUCTS_DIR}"/libWm5AglApplicationd.dylib ../../SDK/Library/DebugDLL/libWm5AglApplicationd.dylib
--- unused_libs/WildMagic5/compile.sh
+++
@@ -1,1 +0,0 @@
-make CFG=Release -f makefile.wm5 -j8 -l8
