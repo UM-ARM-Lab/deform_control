f62f698fb9798c95d57b66f35c65172457876fb2
Dale McConachie <mcconachie@cmu.edu>
2015-11-19 16:33:05 -0500

    Added a setup file to install needed libraries.

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:openrave/release
+sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl
+sudo apt-get update
+
+sudo apt-get install cmake libboost-all-dev libeigen3-dev libopenthreads-dev libopenscenegraph-dev openrave libpcl-all
