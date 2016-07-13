4790d0ae005a0acb869736dfe6507f329103fe37
Dale McConachie <dmcconachie@wpi.edu>
2016-07-13 17:24:28 -0400

    Running trials on Azure.

--- deform_simulator/src/custom_scene/custom_scene.cpp
+++ deform_simulator/src/custom_scene/custom_scene.cpp
@@ -1734,6 +1734,7 @@ void CustomScene::testGripperPosesExecuteCallback(
     const size_t num_tests = goal->poses_to_test.size();
 
     std::lock_guard<std::mutex> lock (sim_mutex_);
+//    omp_set_num_threads(8);
     #pragma omp parallel for
     for (size_t test_ind = 0; test_ind < num_tests; test_ind++)
     {
